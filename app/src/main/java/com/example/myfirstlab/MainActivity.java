870609c58447d83a77c3114a7e8b49006eed3f5b
Shadow-Legacy <oleksii.parshyn@knf.stud.vu.lt>
2023-10-02 13:02:43 +0300

    Second commit

--- app/src/main/java/com/example/myfirstlab/MainActivity.java
+++ app/src/main/java/com/example/myfirstlab/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.myfirstlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button showBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.Hello);
+        showBtn = (Button) findViewById(R.id.button);
+        showBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("my name is Alex");
+            }
+        });
     }
 }
